1dc84d569dfe0a74db6c8b1bc6b5eeb13aaf42ca
lorenzhart <nekogusa9@gmail.com>
2014-06-10 20:18:02 +0900

    featureを直した。環境で動いていない。

--- themes/wordpress-bootstrap-master 3/index.php
+++ themes/wordpress-bootstrap-master 3/index.php
@@ -12,73 +12,6 @@
 
 <?php get_template_part('featured'); ?>
 
-<!-- miracle topic start -->
-<section>
-	<div class="row">
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<img src="http://i.ytimg.com/vi/cfpX8lkaSdk/0.jpg" alt="alt here..." class="topicthumb" />
-		</div>
-	</div>
-	<div class="row">
-		<div class="topics col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<span class="enheader">MIRACLE TOPICS</span><span class="jpheader">人気記事</span>
-		</div>
-	</div>
-	<article id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix"<?php post_class('clearfix'); ?> role="article">
-		<header>
-			<div class="row">
-				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-					<img class="thumb" src="http://i.ytimg.com/vi/cfpX8lkaSdk/3.jpg" alt="alt here..." />
-				</div>
-				<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-					<div class="row">
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-							<h1 class="topictitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-						</div>
-					</div>
-					<div class="row">
-						<div class="info col-xs-12 col-sm-12 col-md-12 col-lg-12">
-							2014.05.29 <span class="categorytag">オモチャ</span>
-						</div>
-					</div>
-				</div>
-			</div>
-			<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'wpbs-featured' ); ?></a>						
-			<!--<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>-->
-		</header> <!-- end article header -->
-		<footer>
-			<p class="tags"><?php the_tags('<span class="tags-title">' . __("Tags","wpbootstrap") . ':</span> ', ' ', ''); ?></p>	
-		</footer> <!-- end article footer -->
-		<hr />
-	</article> <!-- end article -->
-	<article id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix"<?php post_class('clearfix'); ?> role="article">
-		<header>
-			<div class="row">
-				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-					<img class="thumb" src="http://i.ytimg.com/vi/cfpX8lkaSdk/3.jpg" alt="alt here..." />
-				</div>
-				<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-					<div class="row">
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-							<h1 class="topictitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-						</div>
-					</div>
-					<div class="row">
-						<div class="info col-xs-12 col-sm-12 col-md-12 col-lg-12">
-							2014.05.29 <span class="category">オモチャ</span>
-						</div>
-					</div>
-				</div>
-			</div>
-			<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'wpbs-featured' ); ?></a>						
-			<!--<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>-->
-		</header> <!-- end article header -->
-		<footer>
-			<p class="tags"><?php the_tags('<span class="tags-title">' . __("Tags","wpbootstrap") . ':</span> ', ' ', ''); ?></p>	
-		</footer> <!-- end article footer -->
-		<hr />
-	</article> <!-- end article -->
-</section><!-- end miracle topic-->
 
 <!-- start new topic -->
 <section>
@@ -128,7 +61,6 @@
 	
 		');} ?>
 	<article id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix"<?php post_class('clearfix'); ?> role="article">
-		<header>
 			<div class="row">
 				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
 					<img class="thumb" src="http://i.ytimg.com/vi/cfpX8lkaSdk/3.jpg" alt="alt here..." />
@@ -149,14 +81,6 @@
 			<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'wpbs-featured' ); ?></a>
 			
 			<!--<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>-->
-		</header> <!-- end article header -->
-	
-		<footer>
-
-			<p class="tags"><?php the_tags('<span class="tags-title">' . __("Tags","wpbootstrap") . ':</span> ', ' ', ''); ?></p>
-			
-		</footer> <!-- end article footer -->
-
 	</article>
 
 	<?php endif; ?>
