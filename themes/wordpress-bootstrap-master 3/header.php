ab9c4a47bffea51d3386b793607c002e48d9e5a7
lorenzhart <nekogusa9@gmail.com>
2014-06-11 21:37:05 +0900

    くそcontainerけした。しね

--- themes/wordpress-bootstrap-master 3/header.php
+++ themes/wordpress-bootstrap-master 3/header.php
@@ -56,4 +56,3 @@
 		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=452572048204794&version=v2.0";
 		  fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
-		<div class="container">
