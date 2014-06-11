c9e0a6764aba48b48ad6656ae4b21231852b73ce
lorenzhart <nekogusa9@gmail.com>
2014-06-11 19:09:06 +0900

    no message

--- themes/wordpress-bootstrap-master 3/header.php
+++ themes/wordpress-bootstrap-master 3/header.php
@@ -45,19 +45,6 @@
 	    				</a>
 						<!--<a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>-->
 					</div>
-
-					<div class="collapse navbar-collapse navbar-responsive-collapse">
-						<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
-
-						<?php //if(of_get_option('search_bar', '1')) {?>
-						<form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-							<div class="form-group">
-								<input name="s" id="s" type="text" class="search-query form-control" autocomplete="off" placeholder="<?php _e('Search','wpbootstrap'); ?>" data-provide="typeahead" data-items="4" data-source='<?php echo $typeahead_data; ?>'>
-							</div>
-						</form>
-						<?php //} ?>
-					</div>
-
 				</div> <!-- end .container -->
 			</div> <!-- end .navbar -->
 		</header> <!-- end header -->
@@ -71,5 +58,3 @@
 		  fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
 		<div class="container">
-		<div class="row">
-		<div class="col-xs-12 col-sm-12 col-md-offset-2 col-md-offset-8 col-lg-offset-2 col-lg-8"> <!-- end #page in header -->
--- themes/wordpress-bootstrap-master 3/index.php
+++ themes/wordpress-bootstrap-master 3/index.php
@@ -7,13 +7,13 @@
 <!-- start new topic -->
 <section>
 	<div class="row">
-		<div class="topiclabel col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="topiclabel col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">
 			<span class="enheader">NEW TOPICS</span><span class="jpheader">新着記事</span>
 		</div>
 	</div>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); $loopcounter++;?>
 	<?php if($loopcounter < 3) : ?>
-	<article id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix article-card"<?php post_class('clearfix'); ?> role="article">
+	<article id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8 clearfix article-card"<?php post_class('clearfix'); ?> role="article">
 		<div class="row card-contents">
 			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 " style="height:100%; ">
 				<div class="thumb-area" style="width;100%; height:100%; background-image:url('http://i.ytimg.com/vi/cfpX8lkaSdk/3.jpg'); background-repeat: no-repeat;
@@ -22,12 +22,12 @@
 			</div>
 			<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 card-desc">
 				<div class="row ">
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">
 						<h1 class="topictitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 					</div>
 				</div>
 				<div class="row article-meta">
-					<div class="info col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="info col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">
 						2014.05.29 <span class="category">オモチャ</span>
 					</div>
 				</div>
@@ -42,11 +42,11 @@
 	</section><!-- end new topic -->
 	<section><!-- start all topic -->
 		<div class="row">
-			<div class="topiclabel col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="topiclabel col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">
 				<span class="enheader">ALL TOPICS</span><span class="jpheader">記事一覧</span>
 			</div>
 		</div>');} ?>
-		<article id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix article-card"<?php post_class('clearfix'); ?> role="article">
+		<article id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8 clearfix article-card"<?php post_class('clearfix'); ?> role="article">
 			<div class="row card-contents">
 				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" style="height:100%; ">
 					<div class="thumb-area" style="width;100%; height:100%; background-image:url('http://i.ytimg.com/vi/cfpX8lkaSdk/3.jpg'); background-repeat: no-repeat;
@@ -55,12 +55,12 @@
 				</div>
 				<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 card-desc">
 					<div class="row">
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<div class="col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">
 							<h1 class="topictitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 						</div>
 					</div>
 					<div class="row article-meta">
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<div class="col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">
 							2014.05.29 <span class="category">オモチャ</span>
 						</div>
 					</div>
