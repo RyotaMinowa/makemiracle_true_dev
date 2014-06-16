366b2b70d79f86e9cfe0d88c45ea45c49e955ff6
shuichiro <s.takeda1201@gmail.com>
2014-06-16 23:58:53 +0900

    サムネイルのサイズ、投稿時間とカテゴリーの表示を修正

--- themes/wordpress-bootstrap-master 3/featured.php
+++ themes/wordpress-bootstrap-master 3/featured.php
@@ -55,11 +55,15 @@ foreach ( $myposts as $post ) :setup_postdata( $post ); ?>
 	<article id="post-<?php the_ID(); ?>" class="clearfix article-card"<?php post_class('clearfix'); ?> role="article">
 		<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
 			<div class="row card-contents">
-				<div class="col-xs-3 col-sm-3 col-md-3 thumb-area">
-					<?php the_post_thumbnail(); ?>
-					<!--<div class="thumb-area" style="width;100%; height:100%; background-image:url('<?php echo $image_url[0]; ?>'); background-repeat: no-repeat;
+				<div class="col-xs-3 col-sm-3 col-md-3 thumb-area" style="height:100%;">
+					<?php
+						$image_id = get_post_thumbnail_id();
+						$image_url = wp_get_attachment_image_src($image_id, true);
+					?>
+					<!--<?php the_post_thumbnail(); ?>-->
+					<div class="thumb-area" style="width:100%; height:100%; background-image:url('<?php echo $image_url[0]; ?>'); background-repeat: no-repeat;
 	  -moz-background-size:100% auto; background-position:50% 50%;
-	  background-size:100% auto;background-color:#000;"></div>-->
+	  background-size:100% auto;background-color:#000;"></div>
 				</div>
 				<div class="col-xs-9 col-sm-9 col-md-9 card-desc">
 					<div class="row">
@@ -69,7 +73,14 @@ foreach ( $myposts as $post ) :setup_postdata( $post ); ?>
 					</div>
 					<div class="row article-meta">
 						<div class="info col-xs-12 col-sm-12 col-md-12 article-meta">
-							2014.05.29 <span class="category">オモチャ</span>
+							<?php echo get_post_time('Y.m.d'); ?>
+							<?php
+								$cat = get_the_category();
+								$cat = $cat[0];
+							?>
+							<span class="<?php echo $cat->category_nicename; ?> category">
+								<?php echo $cat->cat_name; ?>
+							</span>
 						</div>
 					</div>
 				</div>
